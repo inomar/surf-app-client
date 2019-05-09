a54beffbe27cb8184d41d8501720fa5f4aeafc0b
MakotoInomata <inomar1486@gmail.com>
2019-05-10 01:47:40 +0900

    feat post style

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PostCard = (props) => {
   const { id, title, image, date, user } = props.post;
 
   return(
-    <div className="box">
-      <div className="p-postCardHead">
-        <figure className="image is-5by4">
-          <img src={image} alt="" className="p-postCard__image u-shadow" />
-        </figure>
-        <h3 className="p-postCard__title title is-4">{title}</h3>
-      </div>
-      <div className="media p-postCardBody">
-        <figure className="media-left">
-          <p className="image is-48x48">
-            <img src={user.image} alt="" className="is-rounded u-shadow" />
-          </p>
-        </figure>
-        <div className="media-content">
-          <div className="content">
-            <p>
-              {user.name}
-              <br />
-              <span>{user.experience}</span>
+    <Link to={`/post/${id}`}>
+      <div className="box">
+        <div className="p-postCardHead">
+          <figure className="image is-5by4">
+            <img src={image} alt="" className="p-postCard__image u-shadow" />
+          </figure>
+          <h3 className="p-postCard__title title is-4">{title}</h3>
+        </div>
+        <div className="media p-postCardBody">
+          <figure className="media-left">
+            <p className="image is-48x48">
+              <img src={user.image} alt="" className="is-rounded u-shadow" />
             </p>
+          </figure>
+          <div className="media-content">
+            <div className="content">
+              <p className="p-postCardBody__title">
+                {user.name}
+                <br />
+                <span className="p-postCardBody__lead">{user.experience}</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,41 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return(
-      <div className="container is-fluid">
-        <section className="hero is-info is-large p-heroBackground">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">
-                Large title
-              </h1>
-              <h2 className="subtitle">
-                Large subtitle
-              </h2>
+      <React.Fragment>
+        <div className="container is-fluid">
+          <section className="hero is-info is-large p-heroBackground">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  Large title
+                </h1>
+                <h2 className="subtitle">
+                  Large subtitle
+                </h2>
+              </div>
+            </div>
+          </section>
+        </div>
+        <section className="section">
+          <div className="container is-fluid">
+            <div className="box">
+              <div className="columns">
+                <div className="column">head</div>
+                <div className="column">body</div>
+              </div>
+            </div>
+            <div className="box">
+              <div className="columns">
+                <div className="column">head</div>
+                <div className="column">body</div>
+              </div>
             </div>
           </div>
         </section>
-      </div>
+        <section className="section p-homeTmp">
+
+        </section>
+      </React.Fragment>
     )
   }
 }
